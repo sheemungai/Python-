a3aaeff74dec22d67f11220a889b36aca1b934c4
sheilla mungai <mungaisheilla19@gmail.com>
2025-03-15 20:42:00 +0300

    new features

--- Week 1/intro.py
+++ Week 1/intro.py
@@ -1,1 +1,15 @@
-print("Hello World!")
+num1=int(input ("enter a number"))
+num2 = int(input("enter another number"))
+operator=input("Enter an operator(+,-,*,/)")
+
+if operator == '+':
+ result = num1 + num2
+elif operator == '-':
+  result = num1 - num2   
+elif operator == '*':
+  result = num1 * num2
+elif operator == '/':
+  result = num1 / num2
+else:
+  result="Invalid Syntax"
+print(f"{num1} {operator} {num2} = {result}")
