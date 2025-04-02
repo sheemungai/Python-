bcbe81122d66ddfb62c455b5d7118c53336559d7
sheilla mungai <mungaisheilla19@gmail.com>
2025-04-02 16:21:23 +0300

    week 3 assignment

---
+++ Week 3/functions.py
@@ -0,0 +1,14 @@
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        discount_amount = (discount_percent/ 100) * price
+        final_price = price - discount_amount
+        return final_price
+    else:
+        return price
+
+price = float(input("Enter the original price :"))
+discount_percent = float(input("Enter the discount percentage: "))
+
+final_price = calculate_discount(price, discount_percent)
+print(f"Final price after discount: {final_price:.2f}")
+    
